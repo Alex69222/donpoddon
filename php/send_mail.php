5ef450adfb384fd6ee75c28c6b1a4136085bfa90
Alex <bbqsauce108@gmail.com>
2021-10-17 21:48:15 +0300

    поставил почту заказчика

--- php/send_mail.php
+++ php/send_mail.php
@@ -37,7 +37,7 @@ try {
     $mail->setFrom('donpoddon-org@yandex.ru', 'DON ПОДДОН'); // Адрес самой почты и имя отправителя
 
     // Получатель письма
-    $mail->addAddress('helloxam3000@gmail.com');  
+    $mail->addAddress('donpoddon36@mail.ru');  
 
     // Прикрипление файлов к письму
 // if (!empty($file['name'][0])) {
